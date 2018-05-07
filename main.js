1324c6c15f0dade05997755923413c0a76d0867d
swkhack <swkhack@gmail.com>
2018-05-07 14:25:27 +0800

    Dependency Injection

--- main.js
+++ main.js
@@ -3,49 +3,12 @@ class Slider{
         this.container = document.getElementById(id)
         this.items = this.container.querySelectorAll(".slider-list__item, .slider-list__item--selected")
         this.cycle = cycle;
-
-        const controller = this.container.querySelector(".slide-list__control")
-        if (controller) {
-            const buttons = controller.querySelectorAll('.slide-list__control-buttons,.slide-list__control-buttons--selected')
-            controller.addEventListener("mouseover",evt=>{
-                const idx = Array.from(buttons).indexOf(evt.target)
-                // console.log(idx)
-                if (idx >= 0) {
-                    this.slideTo(idx)
-                    this.stop()
-                }
-            })
-            controller.addEventListener('mouseout',evt=>{
-                this.start()
-            })
-            this.container.addEventListener('slide',evt=>{
-                const idx = evt.detail.index
-                console.log(idx)
-                const selected = controller.querySelector('.slide-list__control-buttons--selected')
-                if (selected) {
-                    selected.className = 'slide-list__control-buttons'
-                }
-                buttons[idx].className = 'slide-list__control-buttons--selected';
-            })
-        }
-        const previous = this.container.querySelector('.slide-list__previous')
-        if (previous) {
-            previous.addEventListener('click',evt=>{
-                this.stop()
-                this.slidePrevious()
-                this.start()
-                evt.preventDefault()
-            })
-        }
-        const next = this.container.querySelector('.slide-list__next')
-        if (next) {
-            next.addEventListener('click',evt=>{
-                this.stop()
-                this.slideNext()
-                this.start()
-                evt.preventDefault()
-            })
-        }
+    }
+    addEventListener(type, handler){
+        this.container.addEventListener(type, handler)
+    }
+    registerPlugins(...plugins){
+        plugins.forEach(plugin => plugin(this))
     }
     getSelectedItem(){
         const selected = this.container.querySelector(".slider-list__item--selected")
@@ -90,6 +53,52 @@ class Slider{
         clearInterval(this._timer);
     }
 }
+function pluginController(slider) {
+    const controller = slider.container.querySelector(".slide-list__control")
+    if (controller) {
+        const buttons = controller.querySelectorAll('.slide-list__control-buttons, .slide-list__control-buttons--selected');
+        controller.addEventListener("mouseover",evt=>{
+            const idx = Array.from(buttons).indexOf(evt.target);
+            if(idx >= 0){
+                slider.slideTo(idx);
+                slider.stop();
+            }
+        });
+
+        controller.addEventListener('mouseout', evt=>{
+            slider.start()
+        });
+        slider.addEventListener('slide', evt => {
+            const idx = evt.detail.index
+            const selected = controller.querySelector('.slide-list__control-buttons--selected');
+            if(selected) selected.className = 'slide-list__control-buttons'
+            buttons[idx].className = 'slide-list__control-buttons--selected'
+        })
+    }
+}
+function PluginPrevious(slider) {
+    const previous = slider.container.querySelector(".slide-list__previous")
+    if(previous){
+        previous.addEventListener('click', evt => {
+            slider.stop();
+            slider.slidePrevious();
+            slider.start();
+            evt.preventDefault();
+        });
+    }
+}
+function pluginNext(slider){
+    const next = slider.container.querySelector('.slide-list__next');
+    if(next){
+        next.addEventListener('click', evt => {
+            slider.stop();
+            slider.slideNext();
+            slider.start();
+            evt.preventDefault();
+        });
+    }
+}
 
 const slider = new Slider('my-slider');
+slider.registerPlugins(pluginController, PluginPrevious, pluginNext)
 slider.start();
